0276350e98624002e22e0c1d118080d2c7c690c6
a1tzhan <107255025+a1tzhan@users.noreply.github.com>
2024-07-04 10:34:22 +0500

    Add files via upload

---
+++ osoc_level_1/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+   float age = 10;
+   float height = 72;
+
+   printf("I am %8.3f years old.\n", age);
+   printf("I am %-8.3f inches tall.\n", height);
+
+   return 0;
+}
